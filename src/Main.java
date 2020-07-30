0cf289b42106189e4e584a1a20b4a0ec85707f74
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-07-30 15:26:56 +0100

    OOP

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,13 @@ public class Main {
         var textBox1 = new TextBox();
         textBox1.setText("Box 1");
         System.out.println(textBox1.text.toUpperCase());
+
+        var textbox2 = new TextBox();
+        textbox2.setText("Box 2");
+        System.out.println(textbox2.text);
+
+        var textbox3 = new TextBox();
+        textBox1.clear();
+        System.out.println(textBox1.text);
     }
 }
