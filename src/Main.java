a020d5616c43926cbef3401eed250e4305a81882
Zack Amin <ishaq.amin@co-operativebank.co.uk>
2020-07-30 10:53:57 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
+    }
+}
---
+++ src/TextBox.java
@@ -0,0 +1,14 @@
+public class TextBox {
+
+    //This class contains one field and two methods.
+
+    public String text; //field
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void clear() {
+        text = "";
+    }
+}
